acdd88c3874adf501bc08ee14637c919e263ef0c
Jay <jl7895@nyu.edu>
2019-10-26 21:15:44 -0400

    Run debug.py can plot the rewards stored in the reward files.

--- gbvared/debug.py
+++ gbvared/debug.py
@@ -1,22 +1,9 @@
 import random
 import torch
 
-import gbvared.control_variate as cv
+import utils
 
-
-c = cv.ControlVariate()
-
-states = []
-actions = []
-for i in range(20):
-    states.append(torch.rand(10))
-    actions.append(torch.rand(5))
-
-states = torch.stack(states)
-actions = torch.stack(actions)
-
-phi, action_grad = c.get_value(states, actions)
-
-print(phi.shape)
-print(action_grad.shape)
+plotter = utils.Plotter()
+utils.load_data_to_plotter('reward_records.txt', plotter)
+plotter.plot()
 
